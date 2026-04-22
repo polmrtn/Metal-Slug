eadd43ed87bc4e0fe4e2812af23e3566624bb983
Fpatyk7 <paciofilip@gmail.com>
2026-04-22 02:07:00 +0200

    granade sound added

--- Solution/Project1/Grenade.hpp
+++ Solution/Project1/Grenade.hpp
@@ -1,12 +1,9 @@
 ﻿#pragma once
-#include<vector>
 #include <raylib.h>
-#include "Soldier.hpp" 
-#include "LevelMap.hpp" 
 
 class Grenade {
 public:
-    Grenade(Vector2 startPos, Vector2 initialVelocity);
+    Grenade(Vector2 startPos, Vector2 targetPos, float power);
     ~Grenade();
 
     void Update();
@@ -16,24 +13,23 @@ public:
     Rectangle GetHitBox() const;
     Rectangle GetExplosionHitBox() const;
     bool HasExploded() const { return hasExploded; }
-    void CheckCollisionWithSoldiers(std::vector<Soldier>& soldiers);
-    void Explode();
-    void StartExplosion();
-    void CheckCollisionWithBlocks(const std::vector<Block>& blocks);
+    bool HasPlayedSound() const { return soundPlayed; }
+    void SetSoundPlayed(bool val) { soundPlayed = val; }
 
 private:
     Vector2 position;
     Vector2 velocity;
+    Vector2 startPos;
+    Vector2 targetPos;
 
-
-    float gravity = 1000.0f;
+    float gravity = 600.0f;
     bool isActive = true;
     bool hasExploded = false;
     float explosionTimer = 0.0f;
     float explosionDuration = 0.3f;
     float explosionRadius = 50.0f;
     bool hasBounced = false;  // ← Si ya ha rebotado
-    float bounceDamping = 0.3f;  // ← Pérdida de velocidad al rebotar (0.6 = 60% de velocidad)
+    float bounceDamping = 0.6f;  // ← Pérdida de velocidad al rebotar (0.6 = 60% de velocidad)
 
     // Animación de la granada
     static Texture2D texture;  
@@ -43,6 +39,9 @@ private:
     int totalFrames = 16;
     float frameDelay = 0.05f;  // Velocidad de animación
 
+    void CalculateTrajectory(float power);
+    void Explode();
+    bool soundPlayed = false;
 
     // Animación de explosión
     float explosionAnimTimer = 0.0f;
@@ -50,7 +49,6 @@ private:
     int explosionTotalFrames = 27;
     float explosionFrameDelay = 0.03f;  // Velocidad de la explosión
     bool isExploding = false;
-    Vector2 explosionPosition = { 0.0f, 0.0f };
 
-
+    void StartExplosion();
 };
