a4a8e813426a3f55332f03d8fc707186291a4689
polmrtn <polmrtn95@gmail.com>
2026-05-13 20:33:05 +0200

    e

--- Solution/Project1/Player.cpp
+++ Solution/Project1/Player.cpp
@@ -462,7 +462,7 @@ Rectangle Player::GetHitBox() {
 Rectangle Player::GetLeftHitBox() {
     Rectangle mainHitBox = GetHitBox();
     float reducedHeight = mainHitBox.height * 0.4f;
-    float offsetY = (mainHitBox.height - reducedHeight) / 2.0f - 25.0f;
+    float offsetY = (mainHitBox.height - reducedHeight) / 2.0f - 35.0f;
     float hitboxX = mainHitBox.x - (mainHitBox.width * 0.3f);
     return Rectangle{ hitboxX, mainHitBox.y + offsetY, mainHitBox.width * 0.4f, reducedHeight };
 }
@@ -470,7 +470,7 @@ Rectangle Player::GetLeftHitBox() {
 Rectangle Player::GetRightHitBox() {
     Rectangle mainHitBox = GetHitBox();
     float reducedHeight = mainHitBox.height * 0.4f;
-    float offsetY = (mainHitBox.height - reducedHeight) / 2.0f -25.0f;
+    float offsetY = (mainHitBox.height - reducedHeight) / 2.0f - 35.0f;
     float hitboxX = mainHitBox.x + mainHitBox.width - (mainHitBox.width * 0.1f);
     return Rectangle{ hitboxX, mainHitBox.y + offsetY, mainHitBox.width * 0.4f, reducedHeight };
 }
